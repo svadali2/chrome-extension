ec366d5bf20472192de871fb4e600190798c0dc4
Soumitri Vadali <svadali2@illinois.edu>
2020-11-07 17:14:40 +0800

    v1 styling done

--- popup.js
+++ popup.js
@@ -1,2 +1,7 @@
-let appendText = document.getElementById('text');
+fetch('http://ipinfo.io?token=0a2a69d103d979').then(r => r.text()).then(str => {
+    var response = JSON.parse(str);
 
+    var ip = document.getElementById('ip').innerHTML = "IP: " + response["ip"];
+    var address = document.getElementById('address').innerHTML = "Location: " + response.city + ", " + response.region;
+    var details = document.getElementById('details').innerHTML = JSON.stringify(response);
+})
